0220756ec01efda7f2102a8f72ce691e7426aac3
Warkka974 <guillaume.boulay30@gmail.com>
2023-12-14 19:33:13 +0100

    résolution exercice01

---
+++ ex01/Script.sh
@@ -0,0 +1,189 @@
+    1  pwd
+    2  cd desktop
+    3  mkdir github
+    4  cd github
+    5  ls
+    6  git init
+    7  code .
+    8  mkdir ESSCA-git
+    9  cd ESSCA-git
+   10  git init
+   11  touch essca.html
+   12  git add .
+   13  git status
+   14  git commit -m "ESSCA is my home"
+   15  git config --global user.name "Warkka974"
+   16  git config --global user.email guillaume.boulay30@gmail.com
+   17  git commit -m "ESSCA is my home"
+   18  mkdir Ex00 
+   19  cd Ex00
+   20  git clone https://github.com/Warkka974/exam_shell_aglo_b1.git
+   21  pwd
+   22  ls
+   23  cd exam_shell_aglo_b1/
+   24  ls
+   25  cd ex00
+   26  ls
+   27  ls
+   28  cd covers
+   29  ls
+   30  mkdir Nintendo
+   31  mkdir Sony
+   32  mkdir Microsoft
+   33  mkdir Sega
+   34  ls
+   35  cd Nintendo
+   36  mkdir Salon
+   37  ls
+   38  mkdir Portable
+   39  cd Salon
+   40  mv gc_mariokartdoubledash.jpg
+   41  mv --help
+   42  mv gc_mariokartdoubledash.jpg ./Salon
+   43  ls
+   44  cd..
+   45  cd .. 
+   46  cd ..
+   47  cd ..
+   48  cd covers
+   49  mv gc_mariokartdoubledash.jpg ./Nintendo
+   50  cd Nintendo
+   51  ls
+   52  mv gc_mariokartdoubledash.jpg ./Salon
+   53  cd ..
+   54  ls
+   55  cd Sega
+   56  ls
+   57  mkdir Dreamcast
+   58  cd ..
+   59  mv dc_jetgrindradio_10.jpg dc_jetsetradio_eu_front.jpg   dc_shenmue_eu.jpg   dc_shenmueii_5.jpg ./Dreamcast/
+   60   mv dc_jetgrindradio_10.jpg dc_jetsetradio_eu_front.jpg   dc_shenmue_eu.jpg   dc_shenmueii_5.jpg ./Dreamcast
+   61  ls
+   62  mv dc_shenmue ./Sega/Dreamcast/
+   63  mv dc_shenmue_eu.jpg ./Sega/Dreamcast/
+   64  ls
+   65  mv dc_jetgrindradio_10.jpg dc_jetsetradio_eu_front.jpg dc_shenmueii_5.jpg ./Sega/Dreamcast/
+   66  ls
+   67  cd sega
+   68  cd Dreamcast
+   69  ls
+   70  cd ..
+   71  cd ..
+   72  cd Nintendo
+   73  ls
+   74  cd Portable
+   75  ls
+   76  mkdir Gameboy
+   77  mkdir GameboyColor
+   78  ls
+   79  cd ..
+   80  cd..
+   81  cd ..
+   82  mv gameboy.pokmonred.jpg  gb_pokemonblue_eu.jpg gb_pokemongreen_3.jpg gb_pokemonyellow_au.jpg ./Nintendo/Portable/Gameboy/
+   83  ls
+   84  mv gbc_pokemongold.jpg ./Nintendo/Portable/GameboyColor/
+   85  ls
+   86  cd Nintendo
+   87  ls
+   88  cd Salon
+   89  ls
+   90  mkdir Gamecube
+   91  ls
+   92  mv  gc_mariokartdoubledash.jpg ./Gamecube/
+   93  cd ..
+   94  cd ..
+   95  mv gc_supermariosunshine.jpg ./Nintendo/Salon/Gamecube/
+   96  ls
+   97  cd Sony
+   98  mkdir Salon
+   99  cd Salon
+  100  ls
+  101  mkdir Playstation4
+  102  mkdir Playstation5
+  103  ls
+  104  cd
+  105  cd ..
+  106  cd ..
+  107  cd desktop
+  108  cd Users
+  109  cd Desktop
+  110  cd ..
+  111  cd ..
+  112  cd ..
+  113  cd Desktop
+  114  cd ..
+  115  ls
+  116  cd ~
+  117  ls
+  118  cd Desktop
+  119  ls
+  120  cd Exam Algo
+  121  cd 'Exam Algo'
+  122  ls
+  123  cd exam_shell_algo_b1
+  124  ls
+  125  ls
+  126  cd exam_shell_aglo_b1
+  127  ls
+  128  cd ex00
+  129  ls
+  130  cd covers
+  131  ls
+  132  mv playstation_4.assassinscreedodyssey.jpg playstation_4.unchartedthelostlegacy.jpg playstation_4.unchartedthelostlegacy.jpg playstation_4.watchdogs2.jpg ps4_fifa14.jpg ./Sony/Salon/Playstation4
+  133  mv playstation_4.assassinscreedodyssey.jpg ./Sony/Salon/Playstation4
+  134  ls
+  135  Sony
+  136  cd Sony
+  137  ls
+  138  cd Salon
+  139  ls
+  140  cd Playstation4
+  141  ls
+  142  cd ..
+  143  cd ..
+  144  cd ..
+  145  ls
+  146  mv playstation_5.assassinscreedvalhalla.jpg playstation_5.farcry6.jpg playstation_5.residentevilvillage_.jpg ./Sony/Salon/Playstation5
+  147  ls
+  148  cd Microsoft
+  149  ls
+  150  mkdir Salon
+  151  cd Salon
+  152  ls
+  153  mkdir Xbox360
+  154  mkdir Xbox
+  155  ls
+  156  cd ..
+  157  cd ..
+  158  ls
+  159  mv x360_callfoduty3.jpg x360_callofduty2.jpg x360_callofduty4.jpg x360_callofdutyblackopsii.jpg ./Microsoft/Salon/Xbox360/
+  160  ls
+  161  mv xbox_360.fableiii.jpg ./Microsoft/Salon/Xbox360/
+  162  ls
+  163  mv xbox_deadoralive3.jpg xbox_deadtorights.jpg xbox_defjamfightforny.jpg xbox_halo2.jpg xbox_halo_2.jpg xbox_jetsetradiofuture_2.jpg xbox_needforspeedunderground2.jpg ./Microsoft/Salon/Xbox/ 
+  164  ls
+  165  git add .
+  166  cd ..
+  167  cd ..
+  168  ls
+  169  git add .
+  170  git status
+  171  git commit -m "j'ai classer les pochettes"
+  172  git push
+  173  ls
+  174  cd ex01
+  175  ls
+  176  wc dialogue.txt
+  177  wc -l
+  178  ls
+  179  cd ex01
+  180  ls
+  181  cd dialogue.txt
+  182  vim
+  183  vim dialogue.txt
+  184  head dialogue.txt
+  185  tail dialogue.txt
+  186  chmod dialogue.txt
+  187  head -c 100 dialogue.txt
+  188  history
+  189  history > Script.sh
